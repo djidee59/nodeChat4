83888bc05c05c12924ac531a748688312f8668cb
djidee59 <juli1.2vilr@gmail.com>
2017-09-22 19:25:02 +0200

    a
    a

--- serverTP2.js
+++ serverTP2.js
@@ -15,6 +15,7 @@ io.sockets.on('connection', function (socket, pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
+        console.log("xxxxxx");
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
