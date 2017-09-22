25c54f5469b5461c74759961fc9890492ccfd5ad
djidee59 <juli1.2vilr@gmail.com>
2017-09-22 20:05:38 +0200

    a
    a

--- serverTP2.js
+++ serverTP2.js
@@ -4,6 +4,10 @@ var app = require('express')(),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
     fs = require('fs');
 
+// port variable sur heroku
+var port = Number(process.env.PORT || 8080);
+
+
 // Chargement de la page index.html
 app.get('/', function (req, res) {
     console.log("*** JDE *** loading HTML");
@@ -11,7 +15,8 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket, pseudo) {
-    console.log("conexion");
+
+
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
@@ -21,10 +26,11 @@ io.sockets.on('connection', function (socket, pseudo) {
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
+        console.log("*** JDE *** message");
         message = ent.encode(message);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 });
 
-var port = Number(process.env.PORT || 8080);
+
 server.listen(port);
