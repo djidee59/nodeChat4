b5f3ed7d3d1d72e1eb1f6c57aba561843dd4fe97
djidee59 <juli1.2vilr@gmail.com>
2017-09-25 14:38:08 +0200

    e
    e

--- serverTP2.js
+++ serverTP2.js
@@ -20,14 +20,14 @@ io.on('connection', (socket) => {
 
   socket.on('nouveau_client', function(pseudo) {
     socket.pseudo = pseudo;
-    socket.broadcast.emit('nouveau_client', pseudo);
+    io.emit('nouveau_client', pseudo);
     console.log("*** JDE *** pseudo: " + pseudo);
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
    socket.on('message', function (message) {
     console.log("*** JDE *** message reçu: " + message);
-    socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    io.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 
 });
