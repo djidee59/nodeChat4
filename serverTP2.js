77894a1fe7aa25614cca4a073c253f7d07681c84
djidee59 <juli1.2vilr@gmail.com>
2017-09-25 13:48:06 +0200

    a
    a

--- serverTP2.js
+++ serverTP2.js
@@ -16,6 +16,15 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+
+      socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
 });
+
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
