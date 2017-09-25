11dd92b356857a8fb814b492d9a68c2a334890c9
djidee59 <juli1.2vilr@gmail.com>
2017-09-25 11:47:49 +0200

    a
    a

--- serverTP2.js
+++ serverTP2.js
@@ -8,8 +8,6 @@ var app = require('express')(),
 var port = Number(process.env.PORT || 8080);
 
 
-
-
 // Chargement de la page index.html
 app.get('/', function (req, res) {
     console.log("*** JDE *** loading HTML");
@@ -18,24 +16,5 @@ app.get('/', function (req, res) {
 
 
 
-// OLD
-io.sockets.on('connection', function (socket, pseudo) {
-
-
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
-    });
-
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', function (message) {
-        console.log("*** JDE *** message");
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    }); 
-});
-
-
 server.listen(port);
+console.log(`Listening on ${ PORT }`);
